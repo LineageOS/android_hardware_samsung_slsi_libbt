5623d73d2c14f46e77e43fda14e0c037ad37bbf8
Stricted <info@stricted.net>
2025-01-12 11:37:52 +0100

    libbt: convert to bp
    losely based on https://gitlab.com/Linaro/96boards/e850-96/platform/hardware/samsung_slsi/-/tree/android13-e850-96? @ 3f80ca082ee2471ffa4c22f70b131dd6af456de3
    
    Change-Id: I5940a952ddccd97a03c75eb3d45151c137d1f356

---
+++ libbt_vendor.go
@@ -0,0 +1,67 @@
+// Copyright 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package libbt_vendor
+
+import (
+	"android/soong/android"
+	"android/soong/cc"
+	"github.com/google/blueprint/proptools"
+	"strings"
+)
+
+func init() {
+	android.RegisterModuleType("libbt_vendor_defaults", libbtVendorDefaultsFactory)
+}
+
+func libbtVendorDefaultsFactory() android.Module {
+	module := cc.DefaultsFactory()
+	android.AddLoadHook(module, libbtVendorDefaults)
+
+	return module
+}
+
+func libbtVendorDefaults(ctx android.LoadHookContext) {
+	type props struct {
+		Include_dirs []string
+		Cflags       []string
+		Enabled      *bool
+	}
+
+	p := &props{}
+	p.Cflags, p.Include_dirs, p.Enabled = globalDefaults(ctx)
+
+	ctx.AppendProperties(p)
+}
+
+func globalDefaults(ctx android.BaseContext) ([]string, []string, *bool) {
+	var cflags []string
+	var includeDirs []string
+	var enabled *bool
+
+	board_bt_buildcfg_include_dir := ctx.DeviceConfig().BtConfigIncludeDir()
+	if len(board_bt_buildcfg_include_dir) > 0 {
+		cflags = append(cflags, "-DHAS_BDROID_BUILDCFG")
+		board_bt_buildcfg_include_dir_list :=
+			strings.Fields(board_bt_buildcfg_include_dir)
+		for _, buildcfg_dir := range board_bt_buildcfg_include_dir_list {
+			includeDirs = append(includeDirs, buildcfg_dir)
+		}
+	} else {
+		cflags = append(cflags, "-DHAS_NO_BDROID_BUILDCFG")
+	}
+
+	enabled = proptools.BoolPtr(true)
+
+	return cflags, includeDirs, enabled
+}
